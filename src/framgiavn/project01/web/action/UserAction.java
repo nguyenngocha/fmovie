530f0881893425760abffd3fcc7ecfba2d3a7770
nguyenngocha <dodaihoc.abvk@gmail.com>
2017-05-03 09:54:41 +0700

    sign-up-in-out

--- src/framgiavn/project01/web/action/UserAction.java
+++ src/framgiavn/project01/web/action/UserAction.java
@@ -14,12 +14,12 @@ public class UserAction extends ActionSupport {
 
 	// private Logit2 log = Logit2.getInstance(UserAction.class);
 
-	private UserBusiness userBusiness = null;
+//	private UserBusiness userBusiness = null;
 	private User user = null;
 
-	public void setUserBusiness(UserBusiness userBusiness) {
-		this.userBusiness = userBusiness;
-	}
+//	public void setUserBusiness(UserBusiness userBusiness) {
+//		this.userBusiness = userBusiness;
+//	}
 
 	public User getUser() {
 		return user;
@@ -29,23 +29,23 @@ public class UserAction extends ActionSupport {
 		this.user = user;
 	}
 
-	public String findByUserId() {
-		try {
-			user = userBusiness.findByUserId(user.getUser_id());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return SUCCESS;
-	}
-
-	public String findByUsername() {
-		try {
-			user = userBusiness.findByUsername(user.getUsername());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return SUCCESS;
-	}
+//	public String findByUserId() {
+//		try {
+//			user = userBusiness.findByUserId(user.getUser_id());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return SUCCESS;
+//	}
+//
+//	public String findByUsername() {
+//		try {
+//			user = userBusiness.findByUsername(user.getUsername());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return SUCCESS;
+//	}
 
 	public String homePage() {
 		System.out
